f23661bcf5c01aa01f7165c5d734f69f7eaccf4e
Your Name <gerhardlr@gmail.com>
2020-01-16 08:44:10 +0000

    added testfile

--- tests.py
+++ tests.py
@@ -11,8 +11,53 @@ import sys
 sys.path.append('/app')
 
 import pytest
+from time import sleep
 from assertpy import assert_that
-from oet.domain import SKAMid, SubArray, ResourceAllocation, Dish
+from oet.domaim import SKAMid, SubArray, ResourceAllocation, Dish
+from tango import DeviceProxy
 
-def test_halo_world():
-    assert_that(1).is_equal_to(1)
+def pause():
+    sleep(4)
+
+
+def test_allocation():
+    the_telescope = SKAMid()
+    the_subarray = SubArray(1)
+    the_resource_allocation = ResourceAllocation(dishes=[Dish(1), Dish(2)])
+
+    print("Starting up telescope ...")
+    the_telescope.start_up()
+
+    print("Releasing any previously allocated resources... ")
+    result = the_subarray.deallocate()
+    pause()
+
+    print("Allocating new resources... ")
+    result = the_subarray.allocate(the_resource_allocation)
+    pause()
+
+    assert_that(result).is_equal_to(the_resource_allocation)
+
+    # Confirm result via direct inspection of TMC
+    subarray_proxy = DeviceProxy('ska_mid/tm_subarray_node/1')
+    pause()
+    receptor_list = subarray_proxy.receptorIDList
+    pause()
+
+    assert_that(receptor_list).is_equal_to("(1, 2)")
+
+    print("Now deallocating resources ... ")
+    subarray.deallocate()
+    pause()
+
+    # Confirm result via direct inspection of TMC - expecting None 
+    receptor_list = subarray_proxy.receptorIDList
+    pause()
+    
+    assert_that(receptor_list).is_equal_to(None)
+
+    print("Subarry has no allocated resources")
+
+    # put telescope to standby
+    telescope.standby()
+    print("Script Complete: All resources dealoccated, Telescope is in standby")
